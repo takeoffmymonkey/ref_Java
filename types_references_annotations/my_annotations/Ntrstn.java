1436dbdf94de5e8874a3ad8f89e5fb310f3ebd45
takeoff <galukhin@gmail.com>
2018-06-02 12:46:21 +0300

    type_system

--- types_references_annotations/my_annotations/Ntrstn.java
+++ types_references_annotations/my_annotations/Ntrstn.java
@@ -1,7 +1,10 @@
 package types_references_annotations.my_annotations;
 
 import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
+@Retention(RetentionPolicy.RUNTIME) // аннотации будут видны в рантайме
 @Repeatable(Ntrstns.class)
 public @interface Ntrstn {
     String value() default "";
--- types_references_annotations/my_annotations/Ntrstns.java
+++ types_references_annotations/my_annotations/Ntrstns.java
@@ -1,5 +1,9 @@
 package types_references_annotations.my_annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME) // аннотации будут видны в рантайме
 public @interface Ntrstns {
     Ntrstn[] value();
 }
