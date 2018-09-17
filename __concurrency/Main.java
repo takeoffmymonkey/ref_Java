c5afe8bbf9ed203b5a50ad65e216fab9549b5c76
takeoff <galukhin@gmail.com>
2018-09-17 21:00:19 +0300

    concurrency

--- __concurrency/Main.java
+++ __concurrency/Main.java
@@ -21,8 +21,9 @@ import static __concurrency.ThreadsColor.ANSI_GREEN;
  * - ветки (потоки): "легковесные" процессы
  *      - также предоставляют среду исполнения, но их создание требует меньше ресурсов, чем для
  *      процесса
- *      - существуют внутри процесса и делят его ресурсы между собой
+ *      - существуют внутри процесса и делят его ресурсы между собой (находятся в Heap)
  *          - из-за этого могут возникать конфликты
+ *      - имеет свой Stack для локальных переменных и только она имеет к нему доступ
  *      - изначально есть только ветка main
  *          - другие создаются из нее
  *              - каждая ветка ассоциирована с экземпляром Thread
@@ -62,6 +63,7 @@ import static __concurrency.ThreadsColor.ANSI_GREEN;
  *      - запрещено запускать ветку более 1 раза */
 
 
+/* НИЗКОУРОВНЕВЫЙ API */
 
 /* ФУНКЦИОНАЛЬНЫЙ ИНТЕРФЕЙС RUNNABLE: run()
  * - общий протокол для объектов, которые хотят исполнять свой код (из метода run) в отдельной ветке
@@ -89,6 +91,9 @@ import static __concurrency.ThreadsColor.ANSI_GREEN;
  *      - может быть демоном или пользовательской(setDaemon): JVM может завершить работу только, если
  *      оставшиеся работающие ветки являются демонами
  *          - статус наследуется ветками-наследниками, но можно потом изменить
+ *
+ *      - имеет уникальный идентификационный номер (getId)
+ *          - сохраняется на время жизни и может быть переиспользован после смерти
  *
  * - типичные манипуляции:
  *      - sleep (временное прекращение выполнения ветки) - если нужно:
@@ -125,13 +130,10 @@ import static __concurrency.ThreadsColor.ANSI_GREEN;
  * - (другие) основные методы:
  *      - activeCount(): приблизительное число активных веток у группы текущей ветки и их подгрупп
  *      - checkAccess(): проверка возможности менять текущую ветку работающей веткой
- *      - clone(): выбрасывает CloneNotSupportedException, т.к. Thread не стоит клонировать
  *      - currentThread(): ссылка на текущий выполняющийся объект ветки
  *      - dumpStack(): вывод stack trace текущей ветки в standard error stream.
  *      - enumerate(Thread[] tarray): вернуть в массиве копии (ссылки?) всех веток и подветок у данной
  *      - getAllStackTraces(): map of stack traces всех живых веток
- *      - getId(): идентификатор ветки
- *      - getState(): состояние данной ветки
  *      - getThreadGroup(): группа ветки, которой принадлежит данная ветка
  *      - holdsLock(Object obj): проверка на удержание монитора веткой для указанного объекта
  *      - isAlive(): проверка живости данной ветки
@@ -139,61 +141,100 @@ import static __concurrency.ThreadsColor.ANSI_GREEN;
 
 
 
-
-/* НИЗКОУРОВНЕВЫЙ API */
-
-
-/* ВЫСОКОУРАВНЕВЫЙ API */
-/*java.util.concurrent*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* У каждой ветки свой stack для локальных переменных и только она имеет к нему доступ, но у всех
- * веток есть доступ в heap, где хранятся переменные члена
- * Не всегда возможно использовать разные объекты, чтобы избежать взаимодействия между ветками,
- * например банковский счет должен быть в единственном числе. Чтобы гарантировать, что член класса
- * вызывается только 1 веткой за 1 раз, его нужно синхронизировать. Но только этот член должен быть
- * везде синхронизирован, если ветки с ним работают в разных местах
- * Конструкторы нельзя синхронизировать из-за бессмесленности (у каждой ветки и так будет свой объект)
- * не синхронизировать локальные переменные (они в стеке, другие ветки не имеют к ним доступа итак),
- * кроме строк, из-за пула
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ВЫСОКОУРАВНЕВЫЙ API~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* ПАКЕТ JAVA.UTIL.CONCURRENT: УТИЛИТНЫЕ КЛАССЫ МНОГОПОТОЧНОСТИ
+ * - Executors:
+ *      - интерфейсы:
+ *          - Executor: простой интерфейс, определяющий кастомные thread-like подсистемы, включая
+ *          пулы потоков, асинхронный I/O и легковесные тасковые фреймворки. В зависимости от
+ *          используемого конкретного класса, кот. имплементирует Executor, таски могут исполняться
+ *          в новых ветках, в существующих или в ветке вызывающей execute, и могут выполняться как
+ *          одновременно, так и последовательно
+ *          - ExecutorService: более сложный фреймворк: управляет очередями и планированием тасков
+ *          и позволяет контролированное завершение
+ *              - ScheduledExecutorService: подинтерфейс для отложенного или периодического выполения.
+ *              - предоставляет методы для организации асинхронного выполнения объектов Callable
+ *                  - аналог Runnable, но возвращает результат
+ *          - Future: возвращает результат Callable, позволяет определять, завершилось ли исполнение,
+ *          предоставляет возможность отменять выполнение
+ *          - RunnableFuture: Future с методом run, который при выполении устанавливает свои результаты.
  *
+ *      - имплементации:
+ *          - ThreadPoolExecutor и ScheduledThreadPoolExecutor: предоставляют настраиваемые и гибкие
+ *          пулы потоков
+ *          - Executors: фабричные методы для наиболее часто используемых конфигураций Executor и
+ *          некоторые методы для их использования
+ *          - FutureTask: простая расширяемая имплементация Future
+ *          - ExecutorCompletionService: помогает при координации обработки групповых тасков
+ *          - ForkJoinPool: предоставляет Executor для обработки экземпляров ForkJoinTask
+ *
+ * - Queues:
+ *      - ConcurrentLinkedQueue: эффективная масштабируемая thread-safe неблокирующая FIFO очередь.
+ *      - ConcurrentLinkedDeque: похоже, но дополнительно поддерживает интерфейс Deque.
+ *      - интерфейс BlockingQueue: определяет блокирующие версии put и take
+ *          - имплементации: LinkedBlockingQueue, ArrayBlockingQueue, SynchronousQueue,
+ *          PriorityBlockingQueue, и DelayQueue
+ *              - для разных контекстов: producer-consumer, messaging, parallel tasking и т.д.
+ *          - подинтерфейс BlockingDeque: поддержка FIFO и LIFO (stack-based)
+ *              - имплементация: LinkedBlockingDeque
+ *      -  TransferQueue (и имплементация LinkedTransferQueue): синхронный медод transfer, который
+ *      позволяет продюсеру блокировать ожидающего его консумера
+ *
+ * - Timing:
+ *       - TimeUnit: предоставляет разные гранулярности (включая наносекунды) для операций таймаута
+ *       или указания времени
+ *
+ * - Synchronizers:
+ *      - Semaphore: классическая тулза многопоточности
+ *      - CountDownLatch: очень простая утилита для блокирования пока происходит указанное число
+ *      сигналов, событий или условий.
+ *      - CyclicBarrier: resettable multiway точка синхронизации.
+ *      - Phaser: более гибкая форма барьера, которая может использоваться для контролирования phased
+ *      вичислений между несколькими ветками.
+ *      - Exchanger: позволяет 2 веткам обмениваться объектами в rendezvous точке.
+ *
+ * - многопоточные коллекции (помимо Queue):
+ *      - ConcurrentHashMap, ConcurrentSkipListMap, ConcurrentSkipListSet, CopyOnWriteArrayList,
+ *      CopyOnWriteArraySet
+ *          - предпочтительней, чем синхронизованные версии коллекций
+ *              - thread-safe, но не управляются 1 монитором
+ *      - их итераторы и сплитераторы предоставляют weakly consistent, а не fast-fail перебор */
+
+
+/* ПАКЕТ JAVA.UTIL.CONCURRENT.LOCKS: РАСШИРЕНИЕ СТАНДАРТНОГО ФУНКЦИОНАЛА МОНИТОРОВ И ОЖИДАНИЯ
+ * - интерфейс Lock: поддержка блокировки при помощи другой семантики, и которая может использоваться
+ * в неблокирующем контексте, включая алгоритмы hand-over-hand и lock reordering
+ *      - ReentrantLock: основная имплементация
+ * - интерфейс ReadWriteLock: можно использовать общий замок между читателями, но эксклюзивный для
+ * записи
+ *      - ReentrantReadWriteLock: единственная имплементация
+ * - интерфейс Condition: условные переменные, ассоциированные с Lock
+ *      - как у Object.wait, но шире функционал
+ *          - например, несколько разных условий
+ * - класс AbstractQueuedSynchronizer: суперкласс-удобство для определения мониторов и других
+ * синхронизаторов, которые полагаются на очередь блокированных потоков
+ *      - унаследован от AbstractOwnableSynchronizer: помогает записывать поток, который удерживает
+ *      замок.
+ * - класс LockSupport: более низкоуравневый, для своих имплементаций */
+
+
+/* ПАКЕТ JAVA.UTIL.CONCURRENT.ATOMIC: АТОМИЧЕСКИЕ ОПЕРАЦИИ НАД ПЕРЕМЕННЫМИ
+ * - по сути классы в пакете расширяет понятие volatile значений, полей и элементов массивов до таких,
+ * что также предоставляют атомическую условную операцию обновления в форме boolean
+ * compareAndSet(expectedValue, updateValue);
+ *      - метод не является общим заменителем блокировки, а применяется только когда критические
+ *      апдейты для объекта заключаются в единственную переменную
+ * - в первую очередь предназначены как строительные элементы для имплементаций неблокирующий
+ * структур данных
+ * - не являются общими заменителями для оберток, так как не определяют методов тех классов
+ * - классы AtomicBoolean, AtomicInteger, AtomicLong, и AtomicReference: доступ и обновление
+ * переменной соответствующего типа, а также другие удобства, например инкрементация
+ * - классы Updater: можно использовать для получения compareAndSet операций для любобо выбранного
+ * volatile поля любого класса
+ * - классы AtomicIntegerArray, AtomicLongArray, and AtomicReferenceArray поддерживают volatile
+ * доступ к элементам массива, что обычным способом не поддерживается
  * */
 
-
-
-
-/*Memory Consistency Properties
-Chapter 17 of the Java Language Specification defines the happens-before relation on memory operations such as reads and writes of shared variables. The results of a write by one thread are guaranteed to be visible to a read by another thread only if the write operation happens-before the read operation. The synchronized and volatile constructs, as well as the Thread.start() and Thread.join() methods, can form happens-before relationships. In particular:
-
-Each action in a thread happens-before every action in that thread that comes later in the program's order.
-An unlock (synchronized block or method exit) of a monitor happens-before every subsequent lock (synchronized block or method entry) of that same monitor. And because the happens-before relation is transitive, all actions of a thread prior to unlocking happen-before all actions subsequent to any thread locking that monitor.
-A write to a volatile field happens-before every subsequent read of that same field. Writes and reads of volatile fields have similar memory consistency effects as entering and exiting monitors, but do not entail mutual exclusion locking.
-A call to start on a thread happens-before any action in the started thread.
-All actions in a thread happen-before any other thread successfully returns from a join on that thread.
-
-The methods of all classes in java.util.concurrent and its subpackages extend these guarantees to higher-level synchronization. In particular:
-
-Actions in a thread prior to placing an object into any concurrent collection happen-before actions subsequent to the access or removal of that element from the collection in another thread.
-Actions in a thread prior to the submission of a Runnable to an Executor happen-before its execution begins. Similarly for Callables submitted to an ExecutorService.
-Actions taken by the asynchronous computation represented by a Future happen-before actions subsequent to the retrieval of the result via Future.get() in another thread.
-Actions prior to "releasing" synchronizer methods such as Lock.unlock, Semaphore.release, and CountDownLatch.countDown happen-before actions subsequent to a successful "acquiring" method such as Lock.lock, Semaphore.acquire, Condition.await, and CountDownLatch.await on the same synchronizer object in another thread.
-For each pair of threads that successfully exchange objects via an Exchanger, actions prior to the exchange() in each thread happen-before those subsequent to the corresponding exchange() in another thread.
-Actions prior to calling CyclicBarrier.await and Phaser.awaitAdvance (as well as its variants) happen-before actions performed by the barrier action, and actions performed by the barrier action happen-before actions subsequent to a successful return from the corresponding await in other threads.*/
 
 @Ntrstn("Вообще у приложения изначально не одна ветка - есть еще системные ветки для управления " +
         "памятью и т.д. Но с точки зрения программиста она 1 - main")
@@ -211,6 +252,7 @@ Actions prior to calling CyclicBarrier.await and Phaser.awaitAdvance (as well as
         "отдельной, в то же время start выполняется в новой ветке и единожды для соотв. объекта Thread")
 
 @Ntrstn("Метод run в Thread есть потому, что он и сам имплементирует Runnable")
+@Ntrstn("бессмысленность синхрониции конструкторов и локальных переменных")
 
 
 public class Main {
--- __concurrency/Synchronization.java
+++ __concurrency/Synchronization.java
@@ -1,5 +1,17 @@
 package __concurrency;
 
+/*
+ * Не всегда возможно использовать разные объекты, чтобы избежать взаимодействия между ветками,
+ * например банковский счет должен быть в единственном числе. Чтобы гарантировать, что член класса
+ * вызывается только 1 веткой за 1 раз, его нужно синхронизировать. Но только этот член должен быть
+ * везде синхронизирован, если ветки с ним работают в разных местах
+ * Конструкторы нельзя синхронизировать из-за бессмесленности (у каждой ветки и так будет свой объект)
+ * не синхронизировать локальные переменные (они в стеке, другие ветки не имеют к ним доступа итак),
+ * кроме строк, из-за пула
+ * */
+
+
+
 /* ПРОБЛЕМЫ МНОГОПОТОЧНОСТИ И ОТНОШЕНИЕ HAPPENS-BEFORE
  * - взаимодействие между ветками приводит к 2 возможным ошибкам:
  *      - thread interference: попытка записи в общие ресурсы несколькими ветками
@@ -9,18 +21,40 @@ package __concurrency;
  *
  * - отношение happens-before: определяет частичный порядок действий в программе - удостоверение, что
  * одно событые точно произошло до начала другого
+ *      - является transitive
  *      - формируется разными действиями, напр:
+ *          - освобождение замка и его же захват
+ *              - и так как transitive, то все действия до освобождения замка произошли до всех
+ *              действий после последующего захвата
+ *          - запись в volatile переменную происходит до чтения той же переменной
+ *              - эффект тот же, что и у замка, но не влечет за собой взаимноисключающий локинг
+ *              - не относится к внутренним ссылкам у данной переменной
+ *              - volatile store + read быстрее, чем блокировка
+ *          - запись значения в final-поле (и, если это поле — ссылка, то ещё и всех переменных,
+ *          достижимых из этого поля (dereference-chain)) при конструировании объекта happens-before
+ *          запись этого объекта в какую-либо переменную, происходящая вне этого конструктора
+ *              - т.е. если есть объект, у которого есть final-поле, то этот объект можно будет
+ *              использовать только после установки этого final-поля (и всего, на что это поле может ссылаться)
+ *              - но если вы передадите из конструктора ссылку на конструируемый объект (т.е. this) наружу, то кто-то может увидеть ваш объект в недостроенном состоянии.
+ *              - т.е. все, что нужно для файнал должно выполниться до записи в нее, т.е. можно без
+ *              волатайл и синхронизации
+ *          - порядок инструкций в 1 ветке
  *          - synchronized
- *          - volatile
  *          - Thread.start()
  *          - Thread.join()
+ *          - semaphore
+ *          - mutex
  *      - например, код в ветке А до создания и запуска ветки Б имеет с веткой Б happens-before
  *      отношение, т.е. ветка Б имеет доступ к коду, который был перед ней в ветке А. После
  *      завершения ветки Б, если в ветке Б был join, то после него доступно все, что было сделано
- *      ветке А */
+ *      ветке А
+ *      - отношение нв транзитивно, если А случилось до б, а б случилось до с, то а случилось до с
+ *      - методы и классы в подпакетах java.util.concurrent расширяют данные гарантии до высокоуравневой
+ *      синхронизации
+ *      */
 
 
-/* СИНХРОНИЗАЦИЯ И THREAD CONTENTION
+/* СИНХРОНИЗАЦИЯ И THREAD СОСТЯЗАНИЕ
  *  - синхронизация призвана предотвратить thread interference и memory consistency errors, но может
  *  вызывать thread contention (соперничество веток). 2 не взаимосключающие формы соперничества:
  *      - starvation: 2 или более ветки пытаются получить одновременно доступ к 1 ресурсу
@@ -85,6 +119,43 @@ package __concurrency;
  * */
 
 
+/* if(data == null) {
+synchronized(this) {
+        if(data == null) {
+        data = new Data();
+        }
+        }
+        }
+
+synchronized(this) {
+            if(data == null) {
+                data = new Data();
+            }
+        }
+
+
+        */
+
+
+
+
+/*Memory Consistency Properties
+Chapter 17 of the Java Language Specification defines the happens-before relation on memory operations such as reads and writes of shared variables. The results of a write by one thread are guaranteed to be visible to a read by another thread only if the write operation happens-before the read operation. The synchronized and volatile constructs, as well as the Thread.start() and Thread.join() methods, can form happens-before relationships. In particular:
+
+Each action in a thread happens-before every action in that thread that comes later in the program's order.
+An unlock (synchronized block or method exit) of a monitor happens-before every subsequent lock (synchronized block or method entry) of that same monitor. And because the happens-before relation is transitive, all actions of a thread prior to unlocking happen-before all actions subsequent to any thread locking that monitor.
+A write to a volatile field happens-before every subsequent read of that same field. Writes and reads of volatile fields have similar memory consistency effects as entering and exiting monitors, but do not entail mutual exclusion locking.
+A call to start on a thread happens-before any action in the started thread.
+All actions in a thread happen-before any other thread successfully returns from a join on that thread.
+
+The methods of all classes in java.util.concurrent and its subpackages extend these guarantees to higher-level synchronization. In particular:
+
+Actions in a thread prior to placing an object into any concurrent collection happen-before actions subsequent to the access or removal of that element from the collection in another thread.
+Actions in a thread prior to the submission of a Runnable to an Executor happen-before its execution begins. Similarly for Callables submitted to an ExecutorService.
+Actions taken by the asynchronous computation represented by a Future happen-before actions subsequent to the retrieval of the result via Future.get() in another thread.
+Actions prior to "releasing" synchronizer methods such as Lock.unlock, Semaphore.release, and CountDownLatch.countDown happen-before actions subsequent to a successful "acquiring" method such as Lock.lock, Semaphore.acquire, Condition.await, and CountDownLatch.await on the same synchronizer object in another thread.
+For each pair of threads that successfully exchange objects via an Exchanger, actions prior to the exchange() in each thread happen-before those subsequent to the corresponding exchange() in another thread.
+Actions prior to calling CyclicBarrier.await and Phaser.awaitAdvance (as well as its variants) happen-before actions performed by the barrier action, and actions performed by the barrier action happen-before actions subsequent to a successful return from the corresponding await in other threads.*/
 
 
 import java.util.ArrayList;
